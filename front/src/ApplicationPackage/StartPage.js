be25dd55560818c424a5363dfef30c20cb572360
EMİRHAN KARAKOÇ <emirhankarakoc@yahoo.com>
2023-12-20 16:40:06 +0300

    lkjh

---
+++ front/src/ApplicationPackage/StartPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import background from './assets/image1.png'
+import { Button, Dropdown,Menu } from 'semantic-ui-react'
+import { Link, NavLink } from "react-router-dom";
+
+
+export default function StartPage() {
+  
+  return (
+    <div style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', height: '100vh', fontFamily: 'Inter, Arial, sans-serif', fontWeight: '600' }}>
+
+
+<div>
+  <div className="container">
+    <div className="center margin-top-30px"> 
+    <div><Menu vertical >
+       <Menu.Item as={NavLink} to="/denizli" name="Denizli" />        
+        <Menu.Item as={NavLink} to="/baku" name="Baku" />
+      </Menu></div>
+    </div>
+  </div>
+</div>
+
+</div>
+  );
+}
