36cc5c2e57abcac41d0f53ec4f8f90424576aee8
EMİRHAN KARAKOÇ <emirhankarakoc@yahoo.com>
2023-12-28 16:47:47 +0300

    asd

--- src/services/NamazService.js
+++ src/services/NamazService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default class NamazService {
     async getAll(sehirIsmi) {
-        return axios.get("https://deneme-6mf0.onrender.com/getAll/"+sehirIsmi,{ withCredentials: false });
+        return axios.get("https://deneme-6mf0.onrender.com/getAll/"+sehirIsmi,{ withCredentials: true });
       }
      
 
