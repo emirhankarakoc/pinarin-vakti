bc9122caa2050ccb3ce526595c0eb58f7e32b7fe
EMİRHAN KARAKOÇ <emirhankarakoc@yahoo.com>
2024-01-05 11:48:00 +0300

    x

--- src/services/NamazService.js
+++ src/services/NamazService.js
@@ -3,12 +3,11 @@ import axios from 'axios';
 export default class NamazService {
   constructor() {
     this.api = axios.create({
-      baseURL: 'https://deneme-6mf0.onrender.com',
-      withCredentials: true,
+      baseURL: 'http://localhost:8080',
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Access-Control-Allow-Origin': 'https://pv-front-emirhankarakoc.vercel.app', // Add the allowed origin
+        'Access-Control-Allow-Origin': '*', // Add the allowed origin
       },
     });
   }
