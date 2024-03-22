6c759d0a7026fa9fea45767e0b6ebd77872799b5
EMİRHAN KARAKOÇ <emirhankarakoc@yahoo.com>
2024-03-22 19:24:50 +0300

    yeni buton başvurusu

--- Frontend/src/ApplicationPackage/StartPage.js
+++ Frontend/src/ApplicationPackage/StartPage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import background from "./assets/image1.png";
-import { Col, Container, ListGroup, Row } from "react-bootstrap";
+import { Button, Col, Container, ListGroup, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function StartPage() {
+  const redirectToWhatsApp = () => {
+    const whatsappLink =
+      "https://api.whatsapp.com/send?phone=905427333289&text=Selam%C4%B1n%20aleyk%C3%BCm.%20Benim%20ya%C5%9Fad%C4%B1%C4%9F%C4%B1m%20%C5%9Fehri%20de%20ekler%20misiniz%20%3F%20%C5%9Eehir%3A";
+    window.location.href = whatsappLink;
+  };
   return (
     <div
       style={{
@@ -25,6 +31,15 @@ export default function StartPage() {
               </ListGroup.Item>
               <ListGroup.Item action href="/istanbul">
                 Istanbul
+              </ListGroup.Item>
+            </ListGroup>
+            <ListGroup>
+              <ListGroup.Item
+                action
+                variant="success"
+                onClick={redirectToWhatsApp}
+              >
+                Yeni şehir başvurusu için tıkla
               </ListGroup.Item>
             </ListGroup>
           </Col>
